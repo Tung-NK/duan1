99d275c8893922fa3ee087e458ba181b738605a3
Tung-NK <tungnguyenkhac222@gmail.com>
2023-11-14 23:34:01 +0700

    day code lan 1

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+    include 'header.php';
+
+    if(isset($_GET['act'])){
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'listdm':
+                include 'danhmuc/listdm.php';
+                break;
+
+            case 'listsp':
+                include 'sanpham/listsp.php';
+                break;    
+
+        default:
+            include 'home.php';
+            break;
+        }
+    }else{
+        include 'home.php';
+    }
+
+
+    include 'footer.php';
+?>
