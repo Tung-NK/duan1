d99ccce428cdbb309978dee46d468ba9d8c8f010
Duan1-TNS <tungnkph38828@fpt.edu.vn>
2023-11-15 15:07:29 +0700

    test code

--- admin/index.php
+++ admin/index.php
@@ -1,25 +1,28 @@
 <?php
-    include 'header.php';
+include 'header.php';
 
-    if(isset($_GET['act'])){
-        $act = $_GET['act'];
-        switch ($act) {
-            case 'listdm':
-                include 'danhmuc/listdm.php';
-                break;
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+    switch ($act) {
+        case 'listdm':
+            include 'danhmuc/listdm.php';
+            break;
 
-            case 'listsp':
-                include 'sanpham/listsp.php';
-                break;    
+        case 'listsp':
+            include 'sanpham/listsp.php';
+            break;
+
+        case 'addsp':
+            include 'sanpham/addsp.php';
+            break;
 
         default:
             include 'home.php';
             break;
-        }
-    }else{
-        include 'home.php';
     }
+} else {
+    include 'home.php';
+}
 
 
-    include 'footer.php';
-?>
+include 'footer.php';
---
+++ admin/sanpham/addsp.php
@@ -0,0 +1,48 @@
+<form action="index.php?act=addsp" method="post" class="form-horizontal">
+    <div class="card-body">
+        <h4 class="card-title">Personal Info</h4>
+        <div class="form-group row">
+            <label class="col-md-2 text-center m-t-15">Single Select</label>
+            <div class="col-md-9 ">
+                <select name="iddm" class="select2 form-control custom-select" style="width: 100%; height:36px;">
+                    <option>Select</option>
+                    <option value="AZ">Arizona</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="fname" class="col-sm-2 text-center control-label col-form-label">Tên sản phẩm</label>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" id="fname" placeholder="Tên sản phẩm">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="lname" class="col-sm-2 text-center control-label col-form-label">Giá</label>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" id="lname" placeholder="Giá sản phẩm">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-md-2 text-center">Hình ảnh</label>
+            <div class="col-md-9">
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                    <label class="custom-file-label" for="validatedCustomFile"></label>
+                    <div class="invalid-feedback">Example invalid custom file feedback</div>
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="cono1" class="col-sm-2 text-center control-label col-form-label">Mô tả</label>
+            <div class="col-sm-9">
+                <textarea class="form-control"></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="border-top text-center">
+        <div class="card-body">
+            <input type="submit" name="themmoi" class="btn btn-primary" value="Thêm mới">
+            <input type="reset" class="btn btn-danger" value="Nhập lại">
+        </div>
+    </div>
+</form>
